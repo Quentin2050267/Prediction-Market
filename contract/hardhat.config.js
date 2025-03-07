3ceac716d23a97543d758d23f914d1c5c52b1027
pwq <panweiqi2016@125.com>
2025-03-07 20:56:50 +0800

    bug: import path

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -3,6 +3,10 @@ require("@matterlabs/hardhat-zksync-verify");
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
 
+require('dotenv').config({
+  path: '../.env', // 指向最外层的 .env 文件
+});
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   zksolc: {
