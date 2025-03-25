104e449a0bc6629c83eb94bdb53757a961675c96
pwq <panweiqi2016@125.com>
2025-03-25 18:07:11 +0800

    AMM update to PaulRBerg

--- contract/contracts/AMM.sol
+++ contract/contracts/AMM.sol
@@ -1,61 +1,90 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-
+// import "PaulRBerg/prb-math@4.1.0/src/UD60x18.sol";
+import { UD60x18, ud, ln, exp } from "PaulRBerg/prb-math@4.1.0/src/UD60x18.sol";
 
 contract AutomatedMarketMaker {
     mapping(uint256 => uint256) public optionAShares;
-    mapping(uint256 => uint256) public optionBShares; 
-    uint256 public constant PRECISION = 1e18; // 价格计算精度
-    uint256 public b = 100 * PRECISION; // 流动性参数
+    mapping(uint256 => uint256) public optionBShares;
+    uint256 public constant PRECISION = 1e18; // precision of price
+    uint256 public b = 1000 * PRECISION; // liquidity
 
     event LiquidityAdded(uint256 indexed marketId, uint256 amountA, uint256 amountB);
     event LiquidityRemoved(uint256 indexed marketId, uint256 amountA, uint256 amountB);
-    event SharesPurchased(uint256 indexed marketId, address indexed buyer, bool isOptionA, uint256 amount, uint256 price);
 
 
-    /** 
+    /**
      * @dev LSMR
      */
-    function getCost(uint256 _marketId, uint256 qA, uint256 qB) public view returns (uint256) {
+    function getCost(uint256 qA, uint256 qB) public view returns (uint256) {
         uint256 expA = _exp(qA * PRECISION / b);
         uint256 expB = _exp(qB * PRECISION / b);
         return (b * _log(expA + expB)) / PRECISION;
     }
-    
-    function getPrice(uint256 _marketId, bool _isOptionA, uint256 _amount) external view returns (uint256) {
-        uint256 qA = optionAShares[_marketId];
-        uint256 qB = optionBShares[_marketId];
 
-        uint256 costBefore = getCost(_marketId, qA, qB);
+    /**
+    * @dev  `_shares` -> get payment
+    */
+    function getAmount(uint256 qA, uint256 qB, bool _isOptionA, uint256 _shares) external view returns (uint256) {
+        require(qA > 0 && qB > 0, "Invalid market qA/qB state");
+        // uint256 qA = optionAShares[_marketId];
+        // uint256 qB = optionBShares[_marketId];
+
+        uint256 costBefore = getCost( qA, qB);
         uint256 costAfter;
         if (_isOptionA) {
-            costAfter = getCost(_marketId, qA + _amount, qB);
+            costAfter = getCost(qA + _shares, qB);
         } else {
-            costAfter = getCost(_marketId, qA, qB + _amount);
+            costAfter = getCost(qA, qB + _shares);
         }
 
         return costAfter - costBefore;
     }
-    
+
+    function getPrices(uint256 qA, uint256 qB) external view returns (uint256 priceA, uint256 priceB) {
+        // NEXT unit of share
+        // uint256 qA = optionAShares[_marketId];
+        // uint256 qB = optionBShares[_marketId];
+
+        uint256 exp_qA = _exp(qA * PRECISION / b);
+        uint256 exp_qB = _exp(qB * PRECISION / b);
+
+        // dominator
+        uint256 S = exp_qA + exp_qB;
+
+        // get P_A 和 P_B
+        priceA = (exp_qA * PRECISION) / S;  // P_A = e^{qA/b} / S
+        priceB = (exp_qB * PRECISION) / S;  // P_B = e^{qB/b} / S
+
+        return (priceA, priceB);
+    }
+
     /**
-    * @dev 计算用户购买 `_amount` 份额时能获得多少 shares
-    *      公式: shares = C(q_A + _amount, q_B) - C(q_A, q_B)
+    * NOT USED IN CURRENT VER
+    * @dev  `_amount` -> get shares
+    *      formula: _amount = C(q_A + _amount, q_B) - C(q_A, q_B)
     */
     function getShares(uint256 _marketId, bool _isOptionA, uint256 _amount) external view returns (uint256) {
         uint256 qA = optionAShares[_marketId];
         uint256 qB = optionBShares[_marketId];
 
-        uint256 costBefore = getCost(_marketId, qA, qB);
-        uint256 costAfter;
+        uint256 expDeltaC = _exp(_amount * PRECISION / b);
+        uint256 exp_qA = _exp(qA * PRECISION / b);
+        uint256 exp_qB = _exp(qB * PRECISION / b);
+
+        uint256 newExp_qA;
         if (_isOptionA) {
-            costAfter = getCost(_marketId, qA + _amount, qB);
+            newExp_qA = expDeltaC * exp_qA + (expDeltaC - 1) * exp_qB;
         } else {
-            costAfter = getCost(_marketId, qA, qB + _amount);
+            newExp_qA = expDeltaC * exp_qB + (expDeltaC - 1) * exp_qA;
         }
 
-        return costAfter - costBefore; // cost paid by user
+        // Δq_A = b * ln(newExp_qA) - qA
+        uint256 deltaQ = (b * _log(newExp_qA)) / PRECISION - qA;
+
+        return deltaQ;
     }
-    
+
     function addLiquidity(uint256 _marketId, uint256 amountA, uint256 amountB) external {
         require(amountA > 0 && amountB > 0, "Amounts must be greater than 0!");
 
@@ -65,7 +94,7 @@ contract AutomatedMarketMaker {
         emit LiquidityAdded(_marketId, amountA, amountB);
     }
 
-    
+
     function removeLiquidity(uint256 _marketId, uint256 amountA, uint256 amountB) external {
         require(optionAShares[_marketId] >= amountA && optionBShares[_marketId] >= amountB, "Not enough liquidity to remove!");
 
@@ -78,34 +107,53 @@ contract AutomatedMarketMaker {
 
     // --- math utility ---
     function _log(uint256 x) internal pure returns (uint256) {
-        require(x > 0, "Log undefined for zero!");
+        require(x > 0, "Log undefined for <= zero!");
 
-        uint256 result = 0;
-        while (x >= 2 * 1e18) {
-            x /= 2;
-            result += 693147180000000000; // approx. log(2) * 1e18
-        }
+        // uint256 -> UD60x18
+        UD60x18 xUD = ud(x);
 
-        uint256 y = (x - 1e18) * 1e18 / (x + 1e18);
-        uint256 y2 = (y * y) / 1e18;
+        UD60x18 result = ln(xUD);
 
-        uint256 term = y;
-        for (uint8 i = 1; i < 10; i += 2) {
-            result += (term / i);
-            term = (term * y2) / 1e18;
-        }
-
-        return 2 * result;
+        return result.unwrap();
     }
     function _exp(uint256 x) internal pure returns (uint256) {
-        uint256 result = PRECISION;
-        uint256 term = PRECISION;
+        // uint256 -> UD60x18
+        UD60x18 xUD = ud(x);
 
-        for (uint8 i = 1; i < 10; i++) {
-            term = (term * x) / (i * PRECISION);
-            result += term;
-        }
-        return result;
+        UD60x18 result = exp(xUD);
+
+        return result.unwrap();
     }
+    // function _log(uint256 x) internal pure returns (uint256) {
+    //     require(x > 0, "Log undefined for <= zero!");
+
+    //     uint256 result = 0;
+    //     while (x >= 2 * 1e18) {
+    //         x /= 2;
+    //         result += 693147180000000000; // approx. log(2) * 1e18
+    //     }
+
+    //     // apporx. between log1-2
+    //     uint256 y = (x - 1e18) * 1e18 / (x + 1e18);
+    //     uint256 y2 = (y * y) / 1e18;
+
+    //     uint256 term = y;
+    //     for (uint8 i = 1; i < 20; i += 2) {
+    //         result += (term / i);
+    //         term = (term * y2) / 1e18;
+    //     }
+
+    //     return result;
+    // }
+    // function _exp(uint256 x) internal pure returns (uint256) {
+    //     uint256 result = PRECISION;
+    //     uint256 term = PRECISION;
+
+    //     for (uint8 i = 1; i < 20; i++) {
+    //         term = (term * x) / (i * PRECISION);
+    //         result += term;
+    //     }
+    //     return result;
+    // }
 
 }
